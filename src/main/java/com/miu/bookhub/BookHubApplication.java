f015720dd9aebb956a4d37f4160ee6fb8d2edc0e
Wendessen <=>
2021-09-06 16:26:40 -0500

    intial checkin

---
+++ src/main/java/com/miu/bookhub/BookHubApplication.java
@@ -0,0 +1,13 @@
+package com.miu.bookhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miu/bookhub/BookHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.bookhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
